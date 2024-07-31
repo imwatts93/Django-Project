3866f0446c256e1f63cb2c226513a6e5aa024c9c
imwatts93 <imwatts.93@gmail.com>
2024-07-31 14:36:31 +0900

    Fix url and register page in users app

--- users/views.py
+++ users/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def user_login(request):
     return render(request, 'users/user_login.html')
+
+def user_register(request):
+    return render(request, 'users/user_register.html')
